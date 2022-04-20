c90584ae2b6ced6d25667bc268dee308398ac0a5
Zachary <75510412+Zachary1575@users.noreply.github.com>
2022-04-19 20:34:50 -0400

    Create scrap.js

---
+++ src/scrap.js
@@ -0,0 +1,54 @@
+//Required library
+const puppeteer = require('puppeteer');
+
+async function scraping(url, tag) {
+    try {
+
+        const URL = '' + url;
+        const browser = await puppeteer.launch({headless: false});
+        const page = await browser.newPage()
+
+        await page.goto(URL)
+
+        /*
+        //put in my username
+        await page.$eval('#j_username', el => el.value = 'XXXXXX');
+        //put in my password
+        await page.$eval('#j_password', el => el.value = 'XXXXX');
+
+        //click the button
+        await page.$eval( '.input-submit', form => form.click() );
+
+        //Wait for navigation
+        await page.waitForNavigation();
+        */
+
+        let resultRecord = await page.evaluate( ()=>{
+            let results = [];
+            let items = document.querySelectorAll('' + tag);
+            items.forEach((item) => {
+
+                results.push(item.innerHTML);
+
+            });
+
+            return results
+        });
+
+        console.log(resultRecord);
+
+        await page.screenshot({ path: "./screenshot.png", fullPage: true});
+
+        await browser.close();
+        
+
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+console.log("Ran!");
+
+
+//Create the necessary Json's
